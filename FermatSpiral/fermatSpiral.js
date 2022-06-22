2c3737ca2f787a4950b8d72f0d18e744600ff02c
Andrej <anhr@mail.ru>
2022-06-23 06:13:14 +0700

    FermatSpiral init

---
+++ FermatSpiral/fermatSpiral.js
@@ -0,0 +1,26 @@
+﻿/**
+ * @module FermatSpiral
+ * @description Implementation of Vogel's model of [Fermat's spiral]{@link https://en.wikipedia.org/wiki/Fermat%27s_spiral}
+ * Thanks to [Fermat_Spiral_Vogel_Model]{@link https://github.com/ceme/Fermat_Spiral_Vogel_Model}
+ *
+ * @author [Andrej Hristoliubov]{@link https://anhr.github.io/AboutMe/}
+ *
+ * @copyright 2011 Data Arts Team, Google Creative Lab
+ *
+ * @license under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+*/
+
+class FermatSpiral {
+
+    constructor() {
+
+        console.log( 'FermatSpiral' );
+
+	}
+}
+export default FermatSpiral;
+
