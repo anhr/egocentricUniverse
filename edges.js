c7dad7ffbb92bde7b8e1520443af6164318d90b3
anhr <anhr@mail.ru>
2023-05-12 19:20:13 +0700

    jsdoc

--- edges.js
+++ edges.js
@@ -789,13 +789,25 @@ class Edges extends EgocentricUniverse {
 	/**
 	 * 1D universe or universe edges.
 	 * @param {Options} options See <a href="../../../commonNodeJS/master/jsdoc/Options/Options.html" target="_blank">Options</a>.
-	 * @param {object} [settings] See <b>EgocentricUniverse <a href="./module-EgocentricUniverse-EgocentricUniverse.html" target="_blank">settings</a></b> parameter.
-	 * @param {number} [settings.count=3] 1D Universe edges count. Default universe is triangle with 3 edges.
-	 * @param {Array} [settings.edges] Edges array. Default edges count is <b>settings.count</b>.
-	 * @param {object} [settings.edges.edge] Edges array item is edge.
-	 * @param {Array} [settings.edges.edge.vertices] Array of edge vertices indices. Every edge have two vertices.
-	 * @param {float} [settings.edges.edge.distance] Edge length. Distance between edge vertices.
-	 * @param {number} [settings.faceId=0] Identifier of the array of the edges ids in the <b>settings.object.geometry.indices.faces array</b>.
+	 * @param {object} [classSettings] Edges class settings.
+	 * @param {number} [classSettings.faceId=0] Identifier of the array of the edges ids in the <b>classSettings.settings.object.geometry.indices.faces</b> array.
+	 * @param {object} [classSettings.settings] See <b>EgocentricUniverse <a href="./module-EgocentricUniverse-EgocentricUniverse.html" target="_blank">settings</a></b> parameter.
+	 * @param {object} [classSettings.settings.object] edges object.
+	 * @param {String} [classSettings.settings.object.name] name of universe.
+	 * @param {String} [classSettings.settings.object.color='lime'] color of edges.
+	 * @param {object} [classSettings.settings.object.geometry] Universe geometry.
+	 * @param {object} [classSettings.settings.object.geometry.indices] Array of <b>indices</b> of vertices, edges and faces of universe.
+	 * @param {number} [classSettings.settings.object.geometry.indices.count=3] 1D Universe edges count. Default universe is triangle with 3 edges.
+	 * @param {Array} [classSettings.settings.object.geometry.indices.edges=[{}, {}, {}]] Edges array. Default edges count is <b>classSettings.settings.object.geometry.indices.count</b>.
+	 * @param {object} [classSettings.settings.object.geometry.indices.edges.edge] Edges array item is edge.
+	 * @param {Array} [classSettings.settings.object.geometry.indices.edges.edge.vertices] Array of edge vertices indices. Every edge have two vertices.
+	 * @param {float} [classSettings.settings.object.geometry.indices.edges.edge.distance=1.0] Edge length. Distance between edge vertices.
+	 * @param {Array} [classSettings.settings.object.geometry.indices.faces=[[0, 1, 2]]] Faces array. Every item of the <b>faces</b> array is array of edges indices for current face.
+	 * <pre>
+	 * Example:
+	 * [[0, 2, 3]]
+	 * universe contains three edges with 0, 2 and 3 indice.
+	 * </pre>
 	 **/
 	constructor( options, classSettings={} ) {
 
